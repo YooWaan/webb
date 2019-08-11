de7962f47fdb001aa80adf781749e9bd81ba90fa
YooWaan <wooyoowaan@gmail.com>
2019-08-12 01:18:58 +0900

    add vue env

--- webb.config.js
+++ webb.config.js
@@ -2,6 +2,37 @@ const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
 
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const rules = [
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader'
+  },
+  // this will apply to both plain `.js` files
+  // AND `<script>` blocks in `.vue` files
+  {
+    test: /\.js$/,
+    loader: 'babel-loader'
+  },
+  // this will apply to both plain `.css` files
+  // AND `<style>` blocks in `.vue` files
+  {
+    test: /\.css$/,
+    use: [
+      'vue-style-loader',
+      'css-loader'
+    ]
+  }
+];
+
+
+const plugins = [new webpack.ProgressPlugin(), new VueLoaderPlugin(),
+   new HtmlWebpackPlugin({title:'webb', template:'src/index.html'})];
+
+
 const config = merge(baseConfig, {
   entry: {
     webb: './src/index.js',
@@ -10,7 +41,18 @@ const config = merge(baseConfig, {
     filename: '[name].v1.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules
+  },
+  plugins: plugins,
 
+  resolve: {
+
+    // aliasを追加
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
 });
 
 module.exports = config;
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,22 +2,6 @@ const webpack = require('webpack');
 
 
 /*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-
-/*
  * We've enabled HtmlWebpackPlugin for you! This generates a html
  * page for you when you compile webpack, which will make you start
  * developing and prototyping faster.
@@ -29,7 +13,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
 
-  plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+  plugins: [new webpack.ProgressPlugin()],
 
   module: {
     rules: [{
